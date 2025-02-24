ddc9da19d039383d715ebcf3b3cd5c01848e76fb
BeahSofia <beatriz.20231@poli.ufrj.br>
2025-02-24 18:20:04 -0300

    Aulas 6 e 7

---
+++ M1/Aula 7 - Programação/Atividade de sala/script.js
@@ -0,0 +1,13 @@
+// Seleciona o botão pelo ID
+const botao = document.getElementById('meuBotao');
+
+// Adiciona um evento de clique ao botão
+botao.addEventListener('click', function() {
+    // Exibe um alerta com as mensagens
+    alert(
+        "Hello world\n" +
+        "Hola mundo\n" +
+        "Salve mondo\n" +
+        "Hallo wêreld"
+    );
+});
